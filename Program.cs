79dbf1759611534529c7b167f20262558ea66b99
Carl Fink <cafink@gmail.com>
2025-04-23 15:46:43 -0500

    Set CORS policy

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ builder.Services.AddCors();
 
 var app = builder.Build();
 
+app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
 app.MapGet("/", () => @"[
   { ""name"": ""United States"", ""holidays"": [""Arbor Day"", ""National Donut Day""] },
   { ""name"": ""Botswana"", ""holidays"": [""National Fried Chicken Day"", ""April Fool's Day""] },
